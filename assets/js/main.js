eec4c7eef8ecd775860740b920ff1d53841398bc
genarvaez <ge_naravez@hotmail.com>
2017-06-15 00:05:54 -0400

    terminado hover de links de navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,24 +6,38 @@ window.addEventListener("scroll", function(){
     var teamLink = document.getElementById("team-link");
     var contactLink = document.getElementById("contact-link");
     if (document.documentElement.scrollTop || document.body.scrollTop > 300 && document.body.scrollTop< 1200) {
-        servicesLink.style.background = "red";
+        servicesLink.style.background = "#FFBF00";
     }
     else{
          servicesLink.style.background = "black";
     }
     
     if(document.documentElement.scrollTop || document.body.scrollTop > 1200 && document.body.scrollTop< 2400){
-        portfolioLink.style.background = "red"
+        portfolioLink.style.background = "#FFBF00"
     }
     else{
          portfolioLink.style.background = "black";
     }
 
     if (document.documentElement.scrollTop || document.body.scrollTop > 2400 && document.body.scrollTop< 3600) {
-        aboutLink.style.background = "red";
+        aboutLink.style.background = "#FFBF00";
     }
     else{
          aboutLink.style.background = "black";
+    }
+
+    if (document.documentElement.scrollTop || document.body.scrollTop > 3800 && document.body.scrollTop< 5000) {
+        teamLink.style.background = "#FFBF00";
+    }
+    else{
+         teamLink.style.background = "black";
+    }
+
+     if (document.documentElement.scrollTop || document.body.scrollTop > 5000 && document.body.scrollTop< 6000) {
+        contactLink.style.background = "#FFBF00";
+    }
+    else{
+         contactLink.style.background = "black";
     }
     
 })
